19a74bb9c15c65b14ffe65623b621e1a01fe6f18
janghanna <ewal4291@gmail.com>
2023-04-17 23:41:29 +0900

    feat : 검색 기능 및 정렬 기능 추가

--- src/Page/Chat/Chat.tsx
+++ src/Page/Chat/Chat.tsx
@@ -6,6 +6,7 @@ import '../style.scss'
 const Chat = (props:any) => {
     const [data, setData] = useState<any[]>([]);
     const [search, setSearch] = useState("");
+    const [count, setCount] = useState(0)
     const Navigate = useNavigate();
 
     useEffect(() => {
@@ -19,6 +20,32 @@ const Chat = (props:any) => {
         })  
     },[])
 
+    // 검색
+    const searchMusic = (e:any) => {
+      setSearch(e.target.value)
+    }
+
+    const submitSearch = (e:any) => {
+        setSearch(e.target.value)
+    }
+
+    let sortUp = [...data]
+
+    //오름차순 정렬
+    const ascendingOrder = () => {
+        sortUp.sort((a:any,b:any) => 
+            a.title.label.localeCompare(b.title.label)
+        )
+        setData(sortUp)
+    }
+
+    //내림차순 정렬
+    const descendingOrder = ()=> {
+        sortUp.sort((a:any,b:any) => 
+        b.title.label.localeCompare(a.title.label))
+        // console.log(data)
+        setData(sortUp)
+    }
 
     const goToDetail = () => {
         props.getMusicDetail(data);
@@ -27,37 +54,46 @@ const Chat = (props:any) => {
 
     return(
         <div className="chat">
-            MUSIC TOP 100
             <div className="search">
+                <h3>ALBEM TOP 100</h3>
                 <input
                     placeholder="찾으시는 음악 이름을 입력하세요"
-                   
+                    onChange={searchMusic}
                 />
-                <button>검색</button>
+                <button onClick={submitSearch}>검색</button>
             </div>
             <div className="sort">
-                <button >오름차순</button>
-                <button>내림차순</button>
+                <button onClick={ascendingOrder}>오름차순</button>
+                <button onClick={descendingOrder}>내림차순</button>
             </div>
             <div className="musics">
-            <div>순위</div>
-            {data.filter((music) => {
-                if(search === ""){
-                return music
-            } else if(music.title.label.toLowerCase().includes(search.toLowerCase())){
-                return music
-            }}).map((music:any) => {
-                return(
-                    <Link to={`/detail/${music.id.attributes["im:id"]}`}>
-                        <div onClick={goToDetail}>
-                        <div className="musicInfo">
-                            <img className="img" src={music["im:image"][0].label} alt=""></img>
-                            <div className="name">{music.title.label}</div>
-                        </div>
+                <div className="line"/>
+                <div className="title">
+                    <h6>순위</h6>
+                    <h6>곡-아티스트</h6>
                 </div>
-                    </Link>   
-                )
-            })}
+                
+                {data.filter((music) => {
+                    if(search === ""){
+                    return music
+                } else if(music.title.label.toLowerCase().includes(search.toLowerCase())){
+                    return music
+                }}).map((music:any) => {
+                    return(
+                    <>
+                        <Link to={`/detail/${music.id.attributes["im:id"]}`} className="link">
+                            <div onClick={goToDetail}>
+                                <div className="musicInfo">
+                                    <div>{count}</div>
+                                    <img className="img" src={music["im:image"][0].label} alt=""></img>
+                                    <div className="name">{music.title.label}</div>
+                                </div>
+                            </div>
+                        </Link>
+                    <div className="line2"/>   
+                    </>
+                    )
+                })}
             </div>
         </div>
     )
